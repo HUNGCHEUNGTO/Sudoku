28f5769dee708c7eead5bb545da3fc59cc01b2e7
HUNGCHEUNGTO <138552972+HUNGCHEUNGTO@users.noreply.github.com>
2023-12-23 09:14:03 +0800

    Update Project.c

--- Project.c
+++ Project.c
@@ -1,25 +1,3 @@
-/**
- * ENGG1110 Problem Solving by Programming
- *
- * Course Project
- *
- * I declare that the project here submitted is original
- * except for source material explicitly acknowledged,
- * and that the same or closely related material has not been
- * previously submitted for another course.
- * I also acknowledge that I am aware of University policy and
- * regulations on honesty in academic work, and of the disciplinary
- * guidelines and procedures applicable to breaches of such
- * policy and regulations, as contained in the website.
- *
- * University Guideline on Academic Honesty:
- *   https://www.cuhk.edu.hk/policy/academichonesty/
- *
- * Student Name  : HUNG CHEUNG TO
- * Student ID    : 1155211693
- * Class/Section : B
- * Date          : 13/11/2023
- */
 
 #include <stdio.h>
 /* NO other header files are allowed */
